1c9e4f6e1964c04faca234d3735e938462ab2a0e
Gopi Kishore <kishorepenjendra@gmail.com>
2021-11-02 19:44:29 +0530

    updated

--- index.js
+++ index.js
@@ -47,6 +47,98 @@ function authenticate(req, res, next) {
     }
     
 }
+app.put('/setpassword',async function(req,res){
+    try {
+        let client=await mongoClient.connect(url);
+        let db = await client.db("login");
+        let salt = bcryptjs.genSaltSync(10);
+        let hash = bcryptjs.hashSync(req.body.password, salt)
+        req.body.password = hash;
+        let user = await db.collection("users").findOne({email:req.body.email,forgot:true});
+        console.log(user)
+        if(user){
+        await db.collection("users").findOneAndUpdate({email:req.body.email,forgot:true},{$set:{password:req.body.password,forgot:false}});
+        client.close();
+        res.json({
+            message:true
+        })
+        }
+        else{
+            res.json({
+                message:false
+            })
+        }
+        
+        
+    } catch (error) {
+        res.json("Either reset email not verified or user not found")
+    }
+})
+app.get('/reset/:token',async function(req,res){
+    try {
+        jwt.verify(req.params.token,process.env.JWT_SECRET,async function(error,decoded){
+            let client = await mongoClient.connect(url)
+            let db = client.db("login");
+            let user = await db.collection("users").findOne({_id:mongodb.ObjectId(decoded.id)});
+           
+            if(user){
+                await db.collection("users").findOneAndUpdate({_id:mongodb.ObjectId(decoded.id)},{$set:{forgot:true}});
+                res.redirect(`${process.env.fronthost}/reset`)
+            }            
+            else{
+                res.json({
+                    message:"Wrong token"
+                })
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+app.post('/forgot',async function(req,res){
+    try {
+        let client = await mongoClient.connect(url)
+
+        // Select the DB
+        let db = client.db("login");
+        let email = await db.collection("users").findOne({email:req.body.email});
+        if(!email){
+            await client.close();
+            // console.log("User not Exists")
+            res.json({
+                message:false
+            })
+        }
+        else{
+            console.log(email)
+            let token = jwt.sign({ id: email._id }, process.env.JWT_SECRET)
+        let data = await db.collection("users").findOneAndUpdate({email:req.body.email},{$set:{token,forgot:false}})
+        // Close the Connection
+        await client.close();
+        
+        // console.log(data.insertedId)
+        let transporter = nodemailer.createTransport({
+            service:"hotmail",
+            auth: {
+              user: process.env.user, 
+              pass: process.env.pass,
+            }})
+            let url = `${process.env.host}/reset/${token}`
+            await transporter.sendMail({
+            from:process.env.user,
+            to:req.body.email,
+            subject:"Reset Password!",
+            html:`<h1>Hey there!</h1>
+            Forgot password? Reset here: <a href=${url}>${url}</a>`
+        })
+        console.log("Email sent")
+        res.json({
+            message: true
+        })}
+    } catch (error) {
+        console.log(error)
+    }
+})
 app.post('/addrecipe',[authenticate],async function(req,res){
     try {
         let client = await mongoClient.connect(url);
